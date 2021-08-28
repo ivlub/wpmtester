cda9dc51bd5cfae9209759e0c28fdc7dbd775986
Pofel <75393666+parno-design@users.noreply.github.com>
2021-08-28 21:54:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 #wpm tester
 import time
+import random
+import sys
+import keyboard
+#
+
+print("Start typing to start the timer.")
 
 
-def hovno():
-    for x in range(6):
-        print("hovno")
-    time.sleep(6)
-    print("done")
+class wpmtester:
+    #print out 10 random words from the words.txt file
+    with open("words.txt") as words_file:
+        words_list = [line.strip() for line in words_file]
+    random.shuffle(words_list)
+    hovno = words_list[:10]
+    print(*hovno, sep= " ")
+    output_length = len(hovno)
+    print(output_length)
 
-if __name__ == "__main__":
-    hovno()
+
+
+    #user input
+    def test():
+        while True:
+            user_input = input("")
+
+            if len(user_input) == 10:
+                print("Well done")
+                break
+            
+
+
