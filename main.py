03994c554ea997a4d7a13b9360d9910542bf232c
Pofel <75393666+parno-design@users.noreply.github.com>
2021-08-31 13:58:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,27 +4,38 @@ from threading import Timer
 import random
 import sys
 import keyboard
-#
+import time
 
 
 
 
-class wpmtester:
-    #word amount
-    word_amount = int(input("How many words would you like to type?    "))
 
-    print(">>> Start typing to start the timer.")
+#word amount
+word_amount = int(input("How many words would you like to type?    "))
 
-    #print out random words from the words.txt file
-    with open("words.txt") as words_file:
-        words_list = [line.strip() for line in words_file]
-    random.shuffle(words_list)
-    hovno = words_list[:word_amount]
-    print(*hovno, sep= " ")
-    output_length = len(hovno)
-    print(output_length)
+print("Okay, going to type", word_amount, "words.")
+print(">>> Start typing to start the timer.")
+#print out random words from the words.txt file
+with open("words.txt") as words_file:
+    words_list = [line.strip() for line in words_file]
+random.shuffle(words_list)
+hovno = words_list[:word_amount]
+print(*hovno, sep= " ")
+sracka = hovno[0][:1]
+output_length = len(hovno)
 
-    user_input = input("")
+
+
+#user input
+keyboard.wait(sracka)
+start_time = time.time()
+user_input = input("")
+end_time = time.time()
+print("time elapsed =", end_time - start_time, "seconds")
+
+
+
+
 
   
 
